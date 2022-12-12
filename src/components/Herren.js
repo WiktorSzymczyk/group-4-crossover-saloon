8f08e17a716397f1e9852d8b08b8ae955878459f
nirgluzman <nir.gluzman@gmail.com>
2022-12-12 16:19:00 +0100

    including base info on each service, styling is required

--- src/components/Herren.js
+++ src/components/Herren.js
@@ -1,8 +1,31 @@
 export default function Herren({ services }) {
   console.log(services);
   return (
-    <>
-      <h1>Herren</h1>
-    </>
+    <div className="flex flex-col bg-[#white] rounded-xl p-4 m-4 md:w-[300px] border-2">
+      {services?.map((service) => (
+        <>
+          <div className="flex justify-center">
+            <img src={service.fields.icon.fields.file.url} />
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <h3>{service.fields.category}</h3>
+          </div>
+
+          <div className="flex flex-col justify-between">
+            {service.fields.servicePrice.map((price) => (
+              <>
+                <div className="text-center text-lg m-4 md:flex md:items-center md:justify-center">
+                  <p>{price.split(";")[0]}</p>
+                </div>
+                <div className="text-center text-lg m-4 md:flex md:items-center md:justify-center">
+                  <p>{price.split(";")[1]}</p>
+                </div>
+              </>
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
   );
 }
