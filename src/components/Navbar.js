3eaa63c9e0d0b4424cb9f1ca036fe5355c3b5e4a
koziu33en <wiktor.szymczyk221@gmail.com>
2022-12-12 14:35:26 +0000

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { Stack, Button, Divider } from "@mui/material";
-import logo from "../img/bg.png";
+import logo from "../img/logo.png";
 
 export default function Navbar({ categoryList }) {
   const handleClick = (event) => {};
